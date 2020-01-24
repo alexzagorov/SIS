ab5ea65a8b3da08c5b124a2f2e8ef7b47c54fe68
alexzagorv <alel_zagorov@abv.bg>
2020-01-24 16:17:32 +0200

    Added functionality of sending responses

--- HTTPServer/HTTPServer/Program.cs
+++ HTTPServer/HTTPServer/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HTTPServer
@@ -18,11 +19,30 @@ namespace HTTPServer
 
             while (true)
             {
-                var tcpClient = tcpListener.AcceptTcpClient();
-                using var incomingStream = tcpClient.GetStream();
+                // Reading the request
+
+                var tcpClient = await tcpListener.AcceptTcpClientAsync();
+                using var tcpStream = tcpClient.GetStream();
                 var incomingBytes = new byte[100000];
-                incomingStream.Read(incomingBytes, 0, incomingBytes.Length);
+                await tcpStream.ReadAsync(incomingBytes, 0, incomingBytes.Length);
                 string incomingRequestString = Encoding.UTF8.GetString(incomingBytes, 0, incomingBytes.Length);
+
+                // Sending response
+
+                string responseBody = "<h1>" + "" + "</h1>" + "<h1>" + DateTime.UtcNow + "</h1>";
+                string response = "HTTP/1.1 200 OK" + NewHTTPLine +
+                    "Content-Type: text/html" + NewHTTPLine +
+                    "Server: SoftUniTestServer/1.1" + NewHTTPLine +
+                    "Set-Cookie: user=Alex; Max-Age=3600; HttpOnly" + NewHTTPLine +
+                    "Content-Lenght: " + responseBody.Length + NewHTTPLine +
+                    NewHTTPLine +
+                    responseBody;
+
+                var responseBytes = Encoding.UTF8.GetBytes(response);
+
+                Thread.Sleep(5000);
+
+                await tcpStream.WriteAsync(responseBytes, 0, responseBytes.Length);
 
                 Console.WriteLine(incomingRequestString);
                 Console.WriteLine(new string('=', 50));
