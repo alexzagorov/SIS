3ec619b30fc2e90640ce09c1bcc3a782c3722570
alexzagorv <alel_zagorov@abv.bg>
2020-02-12 18:04:55 +0200

    Finish methods GetInstanceFromCode and PrepapreCSharpCode in VE

--- Source/SIS.MvcFramework/ViewEngine.cs
+++ Source/SIS.MvcFramework/ViewEngine.cs
@@ -2,8 +2,11 @@
 using Microsoft.CodeAnalysis.CSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SIS.MvcFramework
 {
@@ -11,7 +14,7 @@ namespace SIS.MvcFramework
     {
         public string GetHtml(string templateHtml, object moedel)
         {
-            var methiodCode = PrepapreCSharpCode(templateHtml);
+            var methodCode = PrepapreCSharpCode(templateHtml);
             var code = $@"using System;
 using System.Text;
 using System.Linq;
@@ -25,7 +28,7 @@ namespace AppViewNamespace
         public string GetHtml(object model)
         {{
             var html = new StringBuilder();
-            {methiodCode}
+            {methodCode}
             return html.ToString();
         }}
     }}
@@ -51,14 +54,66 @@ namespace AppViewNamespace
 
             compilation.AddSyntaxTrees(SyntaxFactory.ParseSyntaxTree(code));
 
-            compilation.Emit("a.dll");
+            using var memoryStream = new MemoryStream();
 
-            return null;
+            var compilationResult = compilation.Emit(memoryStream);
+            if (!compilationResult.Success)
+            {
+                return new ErrorView(compilationResult.Diagnostics
+                    .Where(x => x.Severity == DiagnosticSeverity.Error)
+                    .Select(x => x.GetMessage()));
+            }
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            var assemblyByteArray = memoryStream.ToArray();
+            var assembly = Assembly.Load(assemblyByteArray);
+            var type = assembly.GetType("AppViewNamespace.AppViewCode");
+            var instance = Activator.CreateInstance(type) as IView;
+
+            return instance;
         }
 
         private string PrepapreCSharpCode(string templateHtml)
         {
-            return string.Empty;
+            var cSharpExpressionRegex = new Regex(@"[^\<\""\s&]+", RegexOptions.Compiled);
+            var supportedOpperators = new[] { "if", "for", "foreach", "else" };
+            StringBuilder cSharpCode = new StringBuilder();
+            StringReader reader = new StringReader(templateHtml);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.TrimStart().StartsWith("{")
+                    || line.TrimStart().StartsWith("}"))
+                {
+                    cSharpCode.AppendLine(line);
+                }
+                else if (supportedOpperators.Any(x => line.TrimStart().StartsWith("@" + x)))
+                {
+                    var indexOfAt = line.IndexOf("@");
+                    line = line.Remove(indexOfAt, 1);
+                    cSharpCode.AppendLine(line);
+                }
+                else
+                {
+                    var currentCSharpLine = new StringBuilder("html.AppendLine(@\"");
+                    while (line.Contains("@"))
+                    {
+                        var atSignLocation = line.IndexOf("@");
+                        var before = line.Substring(0, atSignLocation);
+                        currentCSharpLine.Append(before.Replace("\"", "\"\"") + "\" + ");
+                        var cSharpAndEndOfLine = line.Substring(atSignLocation + 1);
+                        var cSharpExpression = cSharpExpressionRegex.Match(cSharpAndEndOfLine);
+                        currentCSharpLine.Append(cSharpExpression.Value + " + @\"");
+                        var after = cSharpAndEndOfLine.Substring(cSharpExpression.Length);
+                        line = after;
+                    }
+
+                    currentCSharpLine.Append(line.Replace("\"", "\"\"") + "\");");
+                    cSharpCode.AppendLine(currentCSharpLine.ToString());
+                }
+            }
+
+            return cSharpCode.ToString();
         }
     }
 }
